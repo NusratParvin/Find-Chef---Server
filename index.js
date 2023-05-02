4f9f8648c948c270d36a6c1a30b22b78a8ef08d4
NusratParvin <nusrat.official@gmail.com>
2023-05-02 14:13:36 +0400

    data generated

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express=require('express')
 const app = express()
 const port = 5000
+var cors=require('cors')
+
+
+const allChefs = require('./data/allRecipes.json')
+const recipeDetails = require('./data/recipeDetails.json')
+
+app.use(cors())
+
+app.get('/allChefs', (req,res)=>{
+    res.send(allChefs)
+  })
+
+  app.get('/chefRecipes', (req,res)=>{
+    res.send(recipeDetails)
+  })
+
 
 app.get('/', (req, res) => {
     res.send('chef is up')
